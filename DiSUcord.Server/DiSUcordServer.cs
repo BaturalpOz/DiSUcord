e240ea75afe4fb4ac383d22532e07ce8b0177349
BaturalpOz <baturalpozturk2000@gmail.com>
2023-12-17 17:13:28 +0300

    .

---
+++ DiSUcord.Server/DiSUcordServer.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class DiSUcordServer
+{
+    private TcpListener tcpListener;
+    private List<ClientHandler> clients = new List<ClientHandler>();
+    private List<string> subscribedClientsIF100 = new List<string>();
+    private List<string> subscribedClientsSPS101 = new List<string>();
+    private object lockObject = new object();
+
+    public DiSUcordServer(IPAddress ipAddress, int port)
+    {
+        tcpListener = new TcpListener(ipAddress, port);
+    }
+
+    public void Start()
+    {
+        tcpListener.Start();
+        Console.WriteLine($"Server started. Listening on {((IPEndPoint)tcpListener.LocalEndpoint).Port}");
+
+        try
+        {
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                ClientHandler clientHandler = new ClientHandler(this, tcpClient);
+                Thread clientThread = new Thread(new ThreadStart(clientHandler.HandleClient));
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    public void BroadcastMessage(string sender, string message, List<string> subscribers)
+    {
+        lock (lockObject)
+        {
+            foreach (var client in clients)
+            {
+                if (subscribers.Contains(client.Username))
+                {
+                    client.SendMessage($"{sender}: {message}");
+                }
+            }
+        }
+    }
+
+    public void SubscribeClient(string username, string channel)
+    {
+        lock (lockObject)
+        {
+            if (channel == "IF 100" && !subscribedClientsIF100.Contains(username))
+            {
+                subscribedClientsIF100.Add(username);
+            }
+            else if (channel == "SPS 101" && !subscribedClientsSPS101.Contains(username))
+            {
+                subscribedClientsSPS101.Add(username);
+            }
+        }
+    }
+    public List<string> GetSubscribedClientsIF100()
+    {
+        lock (lockObject)
+        {
+            return new List<string>(subscribedClientsIF100);
+        }
+    }
+
+    public List<string> GetSubscribedClientsSPS101()
+    {
+        lock (lockObject)
+        {
+            return new List<string>(subscribedClientsSPS101);
+        }
+    }
+
+    public void UnsubscribeClient(string username, string channel)
+    {
+        lock (lockObject)
+        {
+            if (channel == "IF 100")
+            {
+                subscribedClientsIF100.Remove(username);
+            }
+            else if (channel == "SPS 101")
+            {
+                subscribedClientsSPS101.Remove(username);
+            }
+        }
+    }
+
+    public void AddClient(ClientHandler clientHandler)
+    {
+        lock (lockObject)
+        {
+            clients.Add(clientHandler);
+        }
+    }
+
+    public void RemoveClient(ClientHandler clientHandler)
+    {
+        lock (lockObject)
+        {
+            clients.Remove(clientHandler);
+        }
+    }
+}
+
+public class ClientHandler
+{
+    private TcpClient tcpClient;
+    private DiSUcordServer server;
+    private StreamReader reader;
+    private StreamWriter writer;
+    public string Username { get; private set; }
+
+    public ClientHandler(DiSUcordServer server, TcpClient tcpClient)
+    {
+        this.server = server;
+        this.tcpClient = tcpClient;
+    }
+
+    public void HandleClient()
+    {
+        try
+        {
+            NetworkStream networkStream = tcpClient.GetStream();
+            reader = new StreamReader(networkStream, Encoding.UTF8);
+            writer = new StreamWriter(networkStream, Encoding.UTF8) { AutoFlush = true };
+
+            // Get username from the client
+            Username = reader.ReadLine();
+            Console.WriteLine($"{Username} connected.");
+
+            // Notify the server about the new client
+            server.AddClient(this);
+
+            while (true)
+            {
+                string clientMessage = reader.ReadLine();
+                if (clientMessage == null)
+                {
+                    break;
+                }
+
+                // Process client messages
+                string[] messageParts = clientMessage.Split(':');
+                if (messageParts.Length == 2)
+                {
+                    string channel = messageParts[0].Trim();
+                    string message = messageParts[1].Trim();
+
+                    // Handle channel subscriptions
+                    if (channel == "Subscribe")
+                    {
+                        server.SubscribeClient(Username, message);
+                    }
+                    else if (channel == "Unsubscribe")
+                    {
+                        server.UnsubscribeClient(Username, message);
+                    }
+                    else
+                    {
+                        // Broadcast the message to the relevant subscribers
+                        List<string> subscribers = GetSubscribers(channel);
+                        server.BroadcastMessage(Username, message, subscribers);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            // Cleanup resources and remove the client from the server
+            Console.WriteLine($"{Username} disconnected.");
+            server.RemoveClient(this);
+            tcpClient.Close();
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        writer.WriteLine(message);
+    }
+
+    private List<string> GetSubscribers(string channel)
+    {
+        if (channel == "IF 100")
+        {
+            return server.GetSubscribedClientsIF100();
+        }
+        else if (channel == "SPS 101")
+        {
+            return server.GetSubscribedClientsSPS101();
+        }
+        else
+        {
+            return new List<string>();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1"); // Change to your server IP address
+        int port = 12345; // Change to your desired port number
+
+        DiSUcordServer server = new DiSUcordServer(ipAddress, port);
+        server.Start();
+    }
+}
---
+++ DiSUcord.UI/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+public partial class Form1 : Form
+{
+    private TcpClient tcpClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+    private string username;
+    private TextBox serverIPTextBox;
+    private TextBox serverPortTextBox;
+    private TextBox usernameTextBox;
+    private Button connectButton;
+    private Label connectionStatusLabel;
+    private RichTextBox messagesRichTextBox;
+    private TextBox messageTextBox;
+    private Button sendMessageButton;
+    private Button disconnectButton;
+    public Form1()
+    {
+        InitializeComponent();
+    }
+    private void InitializeComponent()
+    {
+        this.serverIPTextBox = new System.Windows.Forms.TextBox();
+        this.serverPortTextBox = new System.Windows.Forms.TextBox();
+        this.usernameTextBox = new System.Windows.Forms.TextBox();
+        this.connectButton = new System.Windows.Forms.Button();
+        this.connectionStatusLabel = new System.Windows.Forms.Label();
+        this.messagesRichTextBox = new System.Windows.Forms.RichTextBox();
+        this.messageTextBox = new System.Windows.Forms.TextBox();
+        this.sendMessageButton = new System.Windows.Forms.Button();
+        this.disconnectButton = new System.Windows.Forms.Button();
+
+        // Add control initialization code here...
+
+        // TextBox - serverIPTextBox
+        this.serverIPTextBox.Location = new System.Drawing.Point(12, 12);
+        this.serverIPTextBox.Name = "serverIPTextBox";
+        this.serverIPTextBox.Size = new System.Drawing.Size(100, 20);
+        // Add other properties as needed...
+
+        // TextBox - serverPortTextBox
+        this.serverPortTextBox.Location = new System.Drawing.Point(118, 12);
+        this.serverPortTextBox.Name = "serverPortTextBox";
+        this.serverPortTextBox.Size = new System.Drawing.Size(50, 20);
+        // Add other properties as needed...
+
+        // TextBox - usernameTextBox
+        this.usernameTextBox.Location = new System.Drawing.Point(12, 38);
+        this.usernameTextBox.Name = "usernameTextBox";
+        this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+        // Add other properties as needed...
+
+        // Button - connectButton
+        this.connectButton.Location = new System.Drawing.Point(174, 10);
+        this.connectButton.Name = "connectButton";
+        this.connectButton.Size = new System.Drawing.Size(75, 23);
+        // Add other properties as needed...
+        this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+
+        // Label - connectionStatusLabel
+        this.connectionStatusLabel.AutoSize = true;
+        this.connectionStatusLabel.Location = new System.Drawing.Point(255, 15);
+        this.connectionStatusLabel.Name = "connectionStatusLabel";
+        this.connectionStatusLabel.Size = new System.Drawing.Size(0, 13);
+        // Add other properties as needed...
+
+        // RichTextBox - messagesRichTextBox
+        this.messagesRichTextBox.Location = new System.Drawing.Point(12, 64);
+        this.messagesRichTextBox.Name = "messagesRichTextBox";
+        this.messagesRichTextBox.Size = new System.Drawing.Size(300, 200);
+        // Add other properties as needed...
+
+        // TextBox - messageTextBox
+        this.messageTextBox.Location = new System.Drawing.Point(12, 270);
+        this.messageTextBox.Name = "messageTextBox";
+        this.messageTextBox.Size = new System.Drawing.Size(220, 20);
+        // Add other properties as needed...
+
+        // Button - sendMessageButton
+        this.sendMessageButton.Location = new System.Drawing.Point(238, 268);
+        this.sendMessageButton.Name = "sendMessageButton";
+        this.sendMessageButton.Size = new System.Drawing.Size(75, 23);
+        // Add other properties as needed...
+        this.sendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
+
+        // Button - disconnectButton
+        this.disconnectButton.Location = new System.Drawing.Point(255, 38);
+        this.disconnectButton.Name = "disconnectButton";
+        this.disconnectButton.Size = new System.Drawing.Size(75, 23);
+        // Add other properties as needed...
+        this.disconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+
+        // Add other controls as needed...
+
+        // Form1
+        this.Controls.Add(this.serverIPTextBox);
+        this.Controls.Add(this.serverPortTextBox);
+        this.Controls.Add(this.usernameTextBox);
+        this.Controls.Add(this.connectButton);
+        this.Controls.Add(this.connectionStatusLabel);
+        this.Controls.Add(this.messagesRichTextBox);
+        this.Controls.Add(this.messageTextBox);
+        this.Controls.Add(this.sendMessageButton);
+        this.Controls.Add(this.disconnectButton);
+        // Add other controls as needed...
+    }
+
+    private void ConnectButton_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(usernameTextBox.Text))
+        {
+            MessageBox.Show("Please enter a username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        username = usernameTextBox.Text;
+
+        try
+        {
+            tcpClient = new TcpClient(serverIPTextBox.Text, int.Parse(serverPortTextBox.Text));
+            NetworkStream networkStream = tcpClient.GetStream();
+            reader = new StreamReader(networkStream, Encoding.UTF8);
+            writer = new StreamWriter(networkStream, Encoding.UTF8) { AutoFlush = true };
+
+            // Send the username to the server
+            writer.WriteLine(username);
+
+            // Start a thread to receive messages from the server
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessages));
+            receiveThread.Start();
+
+            // Update UI
+            connectionStatusLabel.Text = "Connected";
+            connectionStatusLabel.ForeColor = System.Drawing.Color.Green;
+            connectButton.Enabled = false;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error connecting to the server: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void ReceiveMessages()
+    {
+        try
+        {
+            while (true)
+            {
+                string message = reader.ReadLine();
+                if (message == null)
+                {
+                    break;
+                }
+
+                // Display the received message in the UI
+                DisplayMessage(message);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error receiving messages: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        finally
+        {
+            // Cleanup resources and update UI
+            Disconnect();
+        }
+    }
+
+    private void DisplayMessage(string message)
+    {
+        // Update the UI to display the received message
+        messagesRichTextBox.AppendText(message + Environment.NewLine);
+    }
+
+    private void SendMessageButton_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(messageTextBox.Text))
+        {
+            // Send the message to the server
+            writer.WriteLine($"ChannelName: {messageTextBox.Text}");
+            messageTextBox.Clear();
+        }
+    }
+
+    private void DisconnectButton_Click(object sender, EventArgs e)
+    {
+        Disconnect();
+    }
+
+    private void Disconnect()
+    {
+        if (tcpClient != null)
+        {
+            // Close the connection and update UI
+            tcpClient.Close();
+            connectionStatusLabel.Text = "Disconnected";
+            connectionStatusLabel.ForeColor = System.Drawing.Color.Red;
+            connectButton.Enabled = true;
+        }
+    }
+
+    // Other GUI event handlers and methods...
+}
+
+public partial class DiSUcordClientGUI
+{
+    // Additional GUI components can be added using the designer in Visual Studio.
+    private TextBox serverIPTextBox;
+    private TextBox serverPortTextBox;
+    private TextBox usernameTextBox;
+    private Button connectButton;
+    private Label connectionStatusLabel;
+    private RichTextBox messagesRichTextBox;
+    private TextBox messageTextBox;
+    private Button sendMessageButton;
+    private Button disconnectButton;
+
+    private void InitializeComponent()
+    {
+        // GUI initialization code...
+    }
+}
