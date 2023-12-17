10658fe53f440d7a32d52146b3e7507dbc923ebe
BaturalpOz <baturalpozturk2000@gmail.com>
2023-12-17 21:28:06 +0300

    .

--- DiSUcord.UI/Form1.Designer.cs
+++ DiSUcord.UI/Form1.Designer.cs
@@ -1,229 +1,121 @@
-﻿using System;
-using System.IO;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace DiSUcord.UI
 {
-    private TcpClient tcpClient;
-    private StreamReader reader;
-    private StreamWriter writer;
-    private string username;
-    private TextBox serverIPTextBox;
-    private TextBox serverPortTextBox;
-    private TextBox usernameTextBox;
-    private Button connectButton;
-    private Label connectionStatusLabel;
-    private RichTextBox messagesRichTextBox;
-    private TextBox messageTextBox;
-    private Button sendMessageButton;
-    private Button disconnectButton;
-    public Form1()
+    partial class Form1
     {
-        InitializeComponent();
-    }
-    private void InitializeComponent()
-    {
-        this.serverIPTextBox = new System.Windows.Forms.TextBox();
-        this.serverPortTextBox = new System.Windows.Forms.TextBox();
-        this.usernameTextBox = new System.Windows.Forms.TextBox();
-        this.connectButton = new System.Windows.Forms.Button();
-        this.connectionStatusLabel = new System.Windows.Forms.Label();
-        this.messagesRichTextBox = new System.Windows.Forms.RichTextBox();
-        this.messageTextBox = new System.Windows.Forms.TextBox();
-        this.sendMessageButton = new System.Windows.Forms.Button();
-        this.disconnectButton = new System.Windows.Forms.Button();
+        private System.ComponentModel.IContainer components = null;
 
-        // Add control initialization code here...
-
-        // TextBox - serverIPTextBox
-        this.serverIPTextBox.Location = new System.Drawing.Point(12, 12);
-        this.serverIPTextBox.Name = "serverIPTextBox";
-        this.serverIPTextBox.Size = new System.Drawing.Size(100, 20);
-        // Add other properties as needed...
-
-        // TextBox - serverPortTextBox
-        this.serverPortTextBox.Location = new System.Drawing.Point(118, 12);
-        this.serverPortTextBox.Name = "serverPortTextBox";
-        this.serverPortTextBox.Size = new System.Drawing.Size(50, 20);
-        // Add other properties as needed...
-
-        // TextBox - usernameTextBox
-        this.usernameTextBox.Location = new System.Drawing.Point(12, 38);
-        this.usernameTextBox.Name = "usernameTextBox";
-        this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
-        // Add other properties as needed...
-
-        // Button - connectButton
-        this.connectButton.Location = new System.Drawing.Point(174, 10);
-        this.connectButton.Name = "connectButton";
-        this.connectButton.Size = new System.Drawing.Size(75, 23);
-        // Add other properties as needed...
-        this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
-
-        // Label - connectionStatusLabel
-        this.connectionStatusLabel.AutoSize = true;
-        this.connectionStatusLabel.Location = new System.Drawing.Point(255, 15);
-        this.connectionStatusLabel.Name = "connectionStatusLabel";
-        this.connectionStatusLabel.Size = new System.Drawing.Size(0, 13);
-        // Add other properties as needed...
-
-        // RichTextBox - messagesRichTextBox
-        this.messagesRichTextBox.Location = new System.Drawing.Point(12, 64);
-        this.messagesRichTextBox.Name = "messagesRichTextBox";
-        this.messagesRichTextBox.Size = new System.Drawing.Size(300, 200);
-        // Add other properties as needed...
-
-        // TextBox - messageTextBox
-        this.messageTextBox.Location = new System.Drawing.Point(12, 270);
-        this.messageTextBox.Name = "messageTextBox";
-        this.messageTextBox.Size = new System.Drawing.Size(220, 20);
-        // Add other properties as needed...
-
-        // Button - sendMessageButton
-        this.sendMessageButton.Location = new System.Drawing.Point(238, 268);
-        this.sendMessageButton.Name = "sendMessageButton";
-        this.sendMessageButton.Size = new System.Drawing.Size(75, 23);
-        // Add other properties as needed...
-        this.sendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
-
-        // Button - disconnectButton
-        this.disconnectButton.Location = new System.Drawing.Point(255, 38);
-        this.disconnectButton.Name = "disconnectButton";
-        this.disconnectButton.Size = new System.Drawing.Size(75, 23);
-        // Add other properties as needed...
-        this.disconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
-
-        // Add other controls as needed...
-
-        // Form1
-        this.Controls.Add(this.serverIPTextBox);
-        this.Controls.Add(this.serverPortTextBox);
-        this.Controls.Add(this.usernameTextBox);
-        this.Controls.Add(this.connectButton);
-        this.Controls.Add(this.connectionStatusLabel);
-        this.Controls.Add(this.messagesRichTextBox);
-        this.Controls.Add(this.messageTextBox);
-        this.Controls.Add(this.sendMessageButton);
-        this.Controls.Add(this.disconnectButton);
-        // Add other controls as needed...
-    }
-
-    private void ConnectButton_Click(object sender, EventArgs e)
-    {
-        if (string.IsNullOrEmpty(usernameTextBox.Text))
+        protected override void Dispose(bool disposing)
         {
-            MessageBox.Show("Please enter a username.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
-
-        username = usernameTextBox.Text;
-
-        try
-        {
-            tcpClient = new TcpClient(serverIPTextBox.Text, int.Parse(serverPortTextBox.Text));
-            NetworkStream networkStream = tcpClient.GetStream();
-            reader = new StreamReader(networkStream, Encoding.UTF8);
-            writer = new StreamWriter(networkStream, Encoding.UTF8) { AutoFlush = true };
-
-            // Send the username to the server
-            writer.WriteLine(username);
-
-            // Start a thread to receive messages from the server
-            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessages));
-            receiveThread.Start();
-
-            // Update UI
-            connectionStatusLabel.Text = "Connected";
-            connectionStatusLabel.ForeColor = System.Drawing.Color.Green;
-            connectButton.Enabled = false;
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show($"Error connecting to the server: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
-
-    private void ReceiveMessages()
-    {
-        try
-        {
-            while (true)
+            if (disposing && (components != null))
             {
-                string message = reader.ReadLine();
-                if (message == null)
-                {
-                    break;
-                }
-
-                // Display the received message in the UI
-                DisplayMessage(message);
+                components.Dispose();
             }
+            base.Dispose(disposing);
         }
-        catch (Exception ex)
+
+        private void InitializeComponent()
         {
-            MessageBox.Show($"Error receiving messages: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            this.IncomingMsgBox = new System.Windows.Forms.RichTextBox();
+            this.SendMsgBox = new System.Windows.Forms.TextBox();
+            this.SubBtn = new System.Windows.Forms.Button();
+            this.UnsubBtn = new System.Windows.Forms.Button();
+            this.SendMsgBtn = new System.Windows.Forms.Button();
+            this.DisconnectBtn = new System.Windows.Forms.Button();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IncomingMsgBox
+            // 
+            this.IncomingMsgBox.Location = new System.Drawing.Point(243, 54);
+            this.IncomingMsgBox.Name = "IncomingMsgBox";
+            this.IncomingMsgBox.Size = new System.Drawing.Size(378, 240);
+            this.IncomingMsgBox.TabIndex = 0;
+            this.IncomingMsgBox.Text = "";
+            // 
+            // SendMsgBox
+            // 
+            this.SendMsgBox.Location = new System.Drawing.Point(243, 325);
+            this.SendMsgBox.Name = "SendMsgBox";
+            this.SendMsgBox.Size = new System.Drawing.Size(287, 22);
+            this.SendMsgBox.TabIndex = 1;
+            this.SendMsgBox.TextChanged += new System.EventHandler(this.SendTxtBox_TextChanged);
+            // 
+            // SubBtn
+            // 
+            this.SubBtn.Location = new System.Drawing.Point(81, 118);
+            this.SubBtn.Name = "SubBtn";
+            this.SubBtn.Size = new System.Drawing.Size(107, 38);
+            this.SubBtn.TabIndex = 2;
+            this.SubBtn.Text = "Subscribe";
+            this.SubBtn.UseVisualStyleBackColor = true;
+            this.SubBtn.Click += new System.EventHandler(this.Subscribe_Click);
+            // 
+            // UnsubBtn
+            // 
+            this.UnsubBtn.Location = new System.Drawing.Point(81, 189);
+            this.UnsubBtn.Name = "UnsubBtn";
+            this.UnsubBtn.Size = new System.Drawing.Size(107, 38);
+            this.UnsubBtn.TabIndex = 3;
+            this.UnsubBtn.Text = "Unsubscribe";
+            this.UnsubBtn.UseVisualStyleBackColor = true;
+            this.UnsubBtn.Click += new System.EventHandler(this.UnsubBtn_Click);
+            // 
+            // SendMsgBtn
+            // 
+            this.SendMsgBtn.Location = new System.Drawing.Point(546, 325);
+            this.SendMsgBtn.Name = "SendMsgBtn";
+            this.SendMsgBtn.Size = new System.Drawing.Size(75, 23);
+            this.SendMsgBtn.TabIndex = 4;
+            this.SendMsgBtn.Text = "Send";
+            this.SendMsgBtn.UseVisualStyleBackColor = true;
+            this.SendMsgBtn.Click += new System.EventHandler(this.SendMsgBtn_Click);
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.Location = new System.Drawing.Point(687, 189);
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.Size = new System.Drawing.Size(107, 38);
+            this.DisconnectBtn.TabIndex = 5;
+            this.DisconnectBtn.Text = "Disconnect";
+            this.DisconnectBtn.UseVisualStyleBackColor = true;
+            this.DisconnectBtn.Click += new System.EventHandler(this.DisconnectBtn_Click);
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(687, 118);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(107, 38);
+            this.ConnectBtn.TabIndex = 6;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(881, 442);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.DisconnectBtn);
+            this.Controls.Add(this.SendMsgBtn);
+            this.Controls.Add(this.UnsubBtn);
+            this.Controls.Add(this.SubBtn);
+            this.Controls.Add(this.SendMsgBox);
+            this.Controls.Add(this.IncomingMsgBox);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
-        finally
-        {
-            // Cleanup resources and update UI
-            Disconnect();
-        }
-    }
 
-    private void DisplayMessage(string message)
-    {
-        // Update the UI to display the received message
-        messagesRichTextBox.AppendText(message + Environment.NewLine);
-    }
-
-    private void SendMessageButton_Click(object sender, EventArgs e)
-    {
-        if (!string.IsNullOrEmpty(messageTextBox.Text))
-        {
-            // Send the message to the server
-            writer.WriteLine($"ChannelName: {messageTextBox.Text}");
-            messageTextBox.Clear();
-        }
-    }
-
-    private void DisconnectButton_Click(object sender, EventArgs e)
-    {
-        Disconnect();
-    }
-
-    private void Disconnect()
-    {
-        if (tcpClient != null)
-        {
-            // Close the connection and update UI
-            tcpClient.Close();
-            connectionStatusLabel.Text = "Disconnected";
-            connectionStatusLabel.ForeColor = System.Drawing.Color.Red;
-            connectButton.Enabled = true;
-        }
-    }
-
-    // Other GUI event handlers and methods...
-}
-
-public partial class DiSUcordClientGUI
-{
-    // Additional GUI components can be added using the designer in Visual Studio.
-    private TextBox serverIPTextBox;
-    private TextBox serverPortTextBox;
-    private TextBox usernameTextBox;
-    private Button connectButton;
-    private Label connectionStatusLabel;
-    private RichTextBox messagesRichTextBox;
-    private TextBox messageTextBox;
-    private Button sendMessageButton;
-    private Button disconnectButton;
-
-    private void InitializeComponent()
-    {
-        // GUI initialization code...
+     
+        private RichTextBox IncomingMsgBox;
+        private TextBox SendMsgBox;
+        private Button SubBtn;
+        private Button UnsubBtn;
+        private Button SendMsgBtn;
+        private Button DisconnectBtn;
+        private Button ConnectBtn;
     }
 }
--- DiSUcord.UI/Form1.cs
+++ DiSUcord.UI/Form1.cs
@@ -1,20 +1,135 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DiSUcord.UI
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+        private TcpClient tcpClient;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private Thread receiveThread;
+
+        private void Form1_Load_1(object sender, EventArgs e)
         {
-            InitializeComponent();
+
+        }
+
+        private void Subscribe_Click(object sender, EventArgs e)
+        {
+            // Subscribe to a channel
+            string channel = "IF 100"; // Replace with the actual channel
+            writer.WriteLine($"Subscribe:{channel}");
+
+            // Update UI
+            IncomingMsgBox.AppendText($"Subscribed to {channel}.\n");
+        }
+
+        private void SendTxtBox_TextChanged(object sender, EventArgs e)
+        {
+            SendMsgBtn.Enabled = !string.IsNullOrWhiteSpace(SendMsgBox.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Disconnect when the form is closed
+            if (tcpClient != null && tcpClient.Connected)
+            {
+                DisconnectBtn_Click(sender, e);
+            }
+        }
+        private void UnsubBtn_Click(object sender, EventArgs e)
+        {
+            // Unsubscribe from a channel
+            string channel = "IF 100"; // Replace with the actual channel
+            writer.WriteLine($"Unsubscribe:{channel}");
+
+            // Update UI
+            IncomingMsgBox.AppendText($"Unsubscribed from {channel}.\n");
+        }
+
+        private void SendMsgBtn_Click(object sender, EventArgs e)
+        {
+            // Send a message to the subscribed channel
+            string message = SendMsgBox.Text;
+            writer.WriteLine($"IF 100:{message}"); // Replace with the actual channel
+
+            // Update UI
+            SendMsgBox.Clear();
+            IncomingMsgBox.AppendText($"You: {message}\n");
+        }
+
+        private void DisconnectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Disconnect from the server
+                tcpClient.Close();
+                receiveThread.Abort();
+
+                // Update UI
+                IncomingMsgBox.AppendText("Disconnected from the server.\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Establish connection to the server
+                tcpClient = new TcpClient("127.0.0.1", 12345); // Update with your server IP and port
+                NetworkStream networkStream = tcpClient.GetStream();
+                reader = new StreamReader(networkStream, Encoding.UTF8);
+                writer = new StreamWriter(networkStream, Encoding.UTF8) { AutoFlush = true };
+
+                // Send the username to the server
+                writer.WriteLine("Username"); // Replace "Username" with the actual username
+
+                // Start a thread to receive messages from the server
+                receiveThread = new Thread(ReceiveMessages);
+                receiveThread.Start();
+
+                // Update UI
+                IncomingMsgBox.AppendText("Connected to the server.\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Connection error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+      
+        private void ReceiveMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    if (message == null)
+                    {
+                        break;
+                    }
+
+                    // Update UI with incoming messages
+                    IncomingMsgBox.Invoke((MethodInvoker)delegate
+                    {
+                        IncomingMsgBox.AppendText(message + "\n");
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
